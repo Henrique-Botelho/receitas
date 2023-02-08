bacf3d5a277b8d00c92bb9133005033ebcbe218d
Henrique de Moraes Botelho da Silva <henriquebotelho.imp@gmail.com>
2023-02-08 14:02:41 -0300

    Update index.js

--- src/screens/Receita/index.js
+++ src/screens/Receita/index.js
@@ -6,8 +6,8 @@ import * as ScreenOrientation from 'expo-screen-orientation';
 export default function Receita({route}) {
     return(
         <View style={styles.container}>
-            <View>
-                <Text>{route.params.nome}</Text>
+            <View style={styles.titulo}>
+                <Text style={styles.textTitulo}>{route.params.nome}</Text>
             </View>
             <View style={styles.video}>
                 <YoutubeIframe
@@ -25,13 +25,32 @@ export default function Receita({route}) {
             </View>
 
             <View style={styles.textos}>
-                <Text>Tempo de Preparo: {route.params.tempo_preparo}</Text>
-                <Text>Rendimento: {route.params.rendimento}</Text>
-                <Text>Ingredientes: {route.params.ingredientes}</Text>
-                <Text>Modo de Preparo: {route.params.modo_preparo}</Text>
-                <Text>Autor: {route.params.autor}</Text>
+                <View style={styles.texto1}>
+                    <View style={styles.texto1_1}>
+                        <Text>Tempo de Preparo</Text>
+                        <Text>{route.params.tempo_preparo}</Text>
+                    </View>
+                    <View style={styles.texto1_1}>
+                        <Text>Rendimento</Text>
+                        <Text>{route.params.rendimento}</Text>
+                    </View>
+                </View>
+                <View style={styles.texto2}>
+                    <Text>Ingredientes</Text>
+                </View>
+                <View style={styles.texto3}>
+                    <Text>{route.params.ingredientes}</Text>
+                </View>
+                <View style={styles.texto4}>
+                    <Text>Modo de Preparo</Text>
+                </View>
+                <View style={styles.texto5}>
+                    <Text>{route.params.modo_preparo}</Text>
+                </View>
+                <View style={styles.texto6}>
+                    <Text>Autor: {route.params.autor}</Text>
+                </View>
             </View>
-
         </View>
     );
 }
@@ -39,9 +58,22 @@ export default function Receita({route}) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "space-around",
         alignItems: "center",
         borderWidth: 7
+    },
+    titulo:{
+        backgroundColor: "#E9B440",
+        borderColor: "red",
+        borderWidth: 7,
+        borderRadius: 15,
+        width: "80%",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    textTitulo:{
+        color: "#FFF",
+        fontSize: 30
     },
     video: {
         borderWidth: 7,
@@ -53,6 +85,41 @@ const styles = StyleSheet.create({
         backgroundColor: "#E9B440"
     },
     textos: {
-
+        borderWidth: 7,
+        borderColor: "purple",
+        borderRadius: 15,
+        backgroundColor: "#E9B440",
+        width: "95%"
+    },
+    texto1: {
+        borderWidth: 7,
+        borderColor: "yellow",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center"
+    },
+    texto1_1: {
+        borderWidth: 7,
+        borderColor: "brown",
+    },
+    texto2: {
+        borderWidth: 7,
+        borderColor: "aquamarine",
+    },
+    texto3: {
+        borderWidth: 7,
+        borderColor: "gray",
+    },
+    texto4: {
+        borderWidth: 7,
+        borderColor: "orange",
+    },
+    texto5: {
+        borderWidth: 7,
+        borderColor: "blue",
+    },
+    texto6: {
+        borderWidth: 7,
+        borderColor: "violet",
     }
 });
